446d814286406ae8995892a9790d32b5ee08e276
EC2 Default User <ec2-user@ip-172-31-31-131.ap-southeast-2.compute.internal>
2025-12-29 13:06:57 +0000

    variables2.sh

---
+++ variables2.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+org="google"
+network="192.168.20.0/24"
+in="active"
+servercount="20"
+
+echo "your organization name is $org"
+echo "your organization ID is $network"
+echo "Internet availablity is $in"
+echo "your organization total server is $server"
+echo "thank you so much viewing the details"
