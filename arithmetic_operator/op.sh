8a2b5877163357f971f18741e350ad157496baef
parthigpm123 <parthigpm123@gmail.com>
2025-12-30 15:08:55 +0530

    operator

---
+++ arithmetic_operator/op.sh
@@ -0,0 +1,10 @@
+ADD=$(( 1 + 2 ))
+echo $ADD
+MUL=$(( $ADD * 5 ))
+echo $MUL
+SUB=$(( $MUL - 5 ))
+echo $SUB
+DIV=$(( $SUB / 2 ))
+echo $DIV
+MOD=$(( $DIV % 2 ))
+echo $MOD
---
+++ userinput.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+read -p "please Enter your name: " org
+read -p "please enter ID " nw
+read -p "please your internet status: " inn
+read -p "please enter your server count:" sc
+
+
+echo "your organization name is $org"
+echo "your organization ID is $nw"
+echo "Internet availablity is $inn"
+echo "your organization total server is $sc"
+echo "thank you so much viewing the details"
+
