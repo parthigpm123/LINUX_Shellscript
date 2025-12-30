5462a2feaedd49e675f8f74bc126aeb17de7b595
parthigpm123 <parthigpm123@gmail.com>
2025-12-30 15:49:14 +0000

    expr

---
+++ arithmetic_operator/expr.sh
@@ -0,0 +1,8 @@
+a=20
+b=6
+
+expr "added: $a + $b"
+expr "substraction :$a - $b"
+expr "MUL:$a \* $b"
+expr "DIV:$a / $b"
+expr "MODULO: $a % $b"
--- arithmetic_operator/op.sh
+++ arithmetic_operator/op.sh
@@ -1,10 +1,12 @@
-ADD=$(( 1 + 2 ))
-echo $ADD
-MUL=$(( $ADD * 5 ))
-echo $MUL
-SUB=$(( $MUL - 5 ))
-echo $SUB
-DIV=$(( $SUB / 2 ))
-echo $DIV
-MOD=$(( $DIV % 2 ))
-echo $MOD
+$ number="payilagam"
+[ec2-user@ip-172-31-31-131 ~]$ echo $number
+payilagam
+[ec2-user@ip-172-31-31-131 ~]$ declare -i number
+[ec2-user@ip-172-31-31-131 ~]$ number="parthiban"
+[ec2-user@ip-172-31-31-131 ~]$ echo $number
+0
+[ec2-user@ip-172-31-31-131 ~]$ number=3
+[ec2-user@ip-172-31-31-131 ~]$ echo $number 
+3
+[ec2-user@ip-172-31-31-131 ~]$ number=number+1
+[ec2-user@ip-172-3-31-131 ~]$ echo $number 
