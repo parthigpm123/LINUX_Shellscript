2a63fe4c8a70b712fae0ab398d4cfb0fa027c85d
parthigpm123 <parthigpm123@.com>
2025-12-31 14:46:02 +0200

    m

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 read -p "please enter a value of 'a' " a
 
 if [ "$a" == "https" ]
 then 
         echo "you have selected https service"
+elif [ "$a" == "vcs" ] #multi if condition
+then 
+        echo "you have selected vcs service"
+elif [ "$a" == "svn" ]
+then 
+        echo "you have selected svn service"
+
+
 else 
         echo "you have selected unknown service"       
 fi
