46712a7ffcc97abad90856351b007837702eb226
parthigpm123 <parthigpm123@.com>
2026-01-01 16:05:08 +0530

    m

--- arithmetic_operator/array.sh
+++ arithmetic_operator/array.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 declare -a planet=("jupiter" "mars" "earth")
+
+planet[02]="sun"
 echo "our planet is " ${planet[0]}
 echo "our planet is " ${planet[1]}
 echo "our planet is " ${planet[2]}
