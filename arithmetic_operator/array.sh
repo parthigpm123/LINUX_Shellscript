858314fa9b6ee013e7173ed2d8f0d49d53749a99
parthigpm123 <parthigpm123@.com>
2026-01-01 16:06:10 +0530

    m

--- arithmetic_operator/array.sh
+++ arithmetic_operator/array.sh
@@ -2,6 +2,7 @@
 declare -a planet=("jupiter" "mars" "earth")
 
 planet[02]="sun"
+unset planet[2]
 echo "our planet is " ${planet[0]}
 echo "our planet is " ${planet[1]}
 echo "our planet is " ${planet[2]}
