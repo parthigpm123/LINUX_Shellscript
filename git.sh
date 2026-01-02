1c48eed4c1eac042e427d3245743276b2c35f022
parthigpm123 <parthigpm123@.com>
2026-01-02 10:06:32 +0530

    changescomited

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+read -p "Enter your commit message: " msg
 git add .
-git commit -m 'comited'
+git commit -m "$msg"
 git push origin main
--- whileloop.sh
+++ whileloop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 a=1
 while (( a<6 )) 
 do
