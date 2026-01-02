db2fd3d2b453c260d9ad951b8ca06d08eb3b3a37
parthigpm123 <parthigpm123@.com>
2026-01-02 10:02:51 +0530

    comited

--- forloop.sh
+++ forloop.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
+#syntax 1
 for a in hello world
 do
 echo "hi $a"
 
+done
+
+
+#syntax 2
+b=100
+
+for ((c=5; b>=c; c=$c+1))
+do
+echo "value of c is $c"
 done
---
+++ whileloop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+a=1
+while (( a<6 )) 
+do
+echo $a
+((a=a+1))
+done
